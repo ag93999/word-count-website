cdb724ac5508ac351b9600e9cbde498d7b7327e2
ag93999 <adityagupta93999@gmail.com>
2018-07-13 16:33:47 +0530

    Added docstrings and .gitignore file.

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 
 urlpatterns = [
+    # ex: admin/
     path('admin/', admin.site.urls),
+    # ex: home/
     path('home/', views.homepage, name='home'),
     path('', views.about, name='about'),
+    # ex: count/
     path('count/', views.count, name='count'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 import operator
 
+
 def about(request):
+    """
+    This view is used to set the about page.
+    """
     return render(request, 'about.html')
 
 
 def homepage(request):
+    """
+    This view is used to set the home page where you can enter your text.
+    """
     return render(request, 'home.html')
 
+
 def count(request):
+    """
+    This view is used to set the count page which will show you the text that you entered and the occurrence of each word.
+    """
     fulltext = request.GET['fulltext']
     wordlist = fulltext.split()
 
     worddictionary = {}
     for word in wordlist:
         if word in worddictionary:
-            worddictionary[word]+=1
+            worddictionary[word] += 1
         else:
-            worddictionary[word]=1
+            worddictionary[word] = 1
 
-    sortedwords = sorted(worddictionary.items(), key = operator.itemgetter(1), reverse=True)
+    sortedwords = sorted(worddictionary.items(), key= operator.itemgetter(1), reverse=True)
 
-    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'worddictionary':sortedwords})
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'worddictionary': sortedwords})
